f8ec71af20d4f9644c46f5fc9b937b09193051cc
Wernerammvsu <wernerammvsu@yandex.ru>
2022-02-03 23:46:33 +0300

    Начало работы над проектом.

---
+++ LinkedList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Self_Stack
+{
+    public class LinkedList<T>
+    {
+        private Node<T> _head;
+        
+        public void Add(T value)
+        {
+            _head = 
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Self_Stack
+{
+    public class Stack<T>
+    {
+        private T[] _array;
+
+        public Stack()
+        {
+           
+        }
+
+        public void Push(T x)
+        {
+
+        }
+    }
+}
